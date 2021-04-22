c7361a2af8b1ea6d5dd33656c4d6758d18f18f30
Sandali Mitma Wickramasinghe <61834612+mitma98@users.noreply.github.com>
2021-04-22 11:09:48 +0530

    Update and rename app.py to application.py

--- flaskbackend/application.py
+++ flaskbackend/application.py
@@ -1,7 +1,7 @@
 from flask import Flask , jsonify , request
 from flask_cors import CORS
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 CORS(app)
 
 @app.route('/hello', methods=["POST"])
